8699f964d594f442ac54aa3552591fdb7e92c9c6
Mohammad Zulqurnain <mohammad.zulqurnain@gmail.com>
2019-07-23 10:05:06 +0500

    framework for logging

---
+++ mac/Console.framework/Versions/A/Headers/Console.h
@@ -0,0 +1,19 @@
+//
+//  Console.h
+//  Console
+//
+//  Created by trial on 7/11/19.
+//  Copyright © 2019 trial. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Console.
+FOUNDATION_EXPORT double ConsoleVersionNumber;
+
+//! Project version string for Console.
+FOUNDATION_EXPORT const unsigned char ConsoleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Console/PublicHeader.h>
+
+
